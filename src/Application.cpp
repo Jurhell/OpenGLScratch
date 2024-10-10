45da75ad4fc5abff90f8eea45842463bc0b9c6b9
Jurhell Hughes <somethinghughes@gmail.com>
2024-10-10 16:53:48 -0500

    feat: Added GLEW to files
    Also drew a triangle in the window

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -9,7 +11,7 @@ int main(void)
         return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "OpenGL", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -19,11 +21,44 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error!" << std::endl;
+
+    std::cout << glGetString(GL_VERSION) << std::endl;
+
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f 
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+//#pragma region LegacyTriangle
+//
+//        glBegin(GL_TRIANGLES);
+//        glVertex2f(-0.5f, -0.5f);
+//        glVertex2f( 0.0f,  0.5f);
+//        glVertex2f( 0.5f, -0.5f);
+//        glEnd();
+//
+//#pragma endregion
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
