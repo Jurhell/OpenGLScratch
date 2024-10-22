aef934fea57136e072cab1f847bb46c8f7570935
Jurhell Hughes <somethinghughes@gmail.com>
2024-10-22 14:25:33 -0500

    feat: Added color uniform, vao, and error callback

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,6 +5,68 @@
 #include <sstream>
 #include <string>
 
+//Macros for Cherno's error call
+#define ASSERT(x) if(!(x)) __debugbreak()
+#define GLCall(x) GLClearError();\
+x;\
+ASSERT(GLLogCall(#x, __FILE__, __LINE__))
+
+void GLDebugCallback(
+    GLenum source, 
+    GLenum type, 
+    GLuint id, 
+    GLenum severity, 
+    GLsizei length, 
+    const GLchar* message, 
+    const void* userParam)
+{
+    const char* severityString = "";
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_NOTIFICATION:
+        severityString = "NOTIFICATION";
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        severityString = "LOW";
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        severityString = "MEDIUM";
+        break;
+    case GL_DEBUG_SEVERITY_HIGH:
+        severityString = "HIGH";
+        break;
+    default:
+        severityString = "DEBUG";
+        break;
+    }
+    
+    std::cout << "[OpenGL] [" << severityString << "] " << message << "\n";
+    if (severity == GL_DEBUG_SEVERITY_HIGH)
+    __debugbreak();
+}
+
+void GLFWErrorCallback(int error, const char* description)
+{
+    std::cerr << description << "\n";
+}
+
+//Cherno's error call
+static void GLClearError()
+{
+    while (!glGetError);
+}
+
+static bool GLLogCall(const char* function, const char* file, int line)
+{
+    while (GLenum error = glGetError())
+    {
+        std::cout << "[OpenGL Error] (" << error << "): " << function << 
+            " " << ";" << line << "\n";
+        return false;
+    }
+    return true;
+}
+
 struct ShaderProgramSource
 {
     std::string VertexSource;
@@ -96,6 +158,10 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "OpenGL Test", NULL, NULL);
     if (!window)
@@ -107,10 +173,18 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1);
+
     if (glewInit() != GLEW_OK)
         std::cout << "Error!" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    //GLFW error callback
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    glEnable(GL_DEBUG_OUTPUT);
+    glfwSetErrorCallback(GLFWErrorCallback);
+    glDebugMessageCallback(GLDebugCallback, nullptr);
 
     float positions[] = {
         -0.5f, -0.5f, // 0
@@ -124,10 +198,14 @@ int main(void)
         2, 3, 0
     };
 
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
@@ -142,13 +220,39 @@ int main(void)
     unsigned int shader = createShader(source.VertexSource, source.FragmentSource);
     glUseProgram(shader);
 
+    int location = glGetUniformLocation(shader, "u_Color");
+    ASSERT(location != -1);
+    glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
+
+    glBindVertexArray(0);
+    glUseProgram(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    float r = 0.0f;
+    float increment = 0.05f;
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shader);
+        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferObject);
+
+        glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        if (r > 1.0f)
+            increment = -0.05f;
+        else if (r < 0.0f)
+            increment = 0.05f;
+
+        r += increment;
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
